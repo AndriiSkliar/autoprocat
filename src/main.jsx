9b4e0f8d631b7b66a270f684347c017e43da78cd
Андрей Скляр <andreas.skliar@gmail.com>
2024-03-07 18:21:02 +0200

    upd visible page on github

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter basename="/second-auto">
+      <BrowserRouter basename="/autoprocat">
         <App />
       </BrowserRouter>
     </PersistGate>
