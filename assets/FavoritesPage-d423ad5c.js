37551145862b9f15e361f7b856e570d8f36856b0
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-03-10 09:06:12 +0000

    Deploying to gh-pages from @ AndriiSkliar/autoprocat@c9b2b6b63ed6de0b07bf8e561a2c4b3da785a17b 🚀

--- assets/FavoritesPage-d423ad5c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-1bf9a93a.js";const t=()=>e.jsx(e.Fragment,{children:"FavoritesPage"});export{t as default};
---
+++ assets/FavoritesPage-f3fa3599.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-1ab1aed6.js";const t=()=>e.jsx(e.Fragment,{children:"FavoritesPage"});export{t as default};
