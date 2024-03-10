87e2e5eaf12809026d8d457bd54e6f877f17c3a4
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-03-10 21:17:32 +0000

    Deploying to gh-pages from @ AndriiSkliar/autoprocat@fcbc9ccaa61dd7a9f223ca6ba7151e344a7297c9 🚀

---
+++ assets/FavoritesPage-9f53e2c5.js
@@ -0,0 +1,1 @@
+import{b as o,j as s,L as n}from"./index-4587a325.js";import{d as r,g as i,a as c,N as d,A as l,P as p}from"./PopUp-ae911ca4.js";const u=()=>{const t=o(r),a=o(i),e=o(c);return s.jsxs("main",{children:[t&&s.jsx(n,{}),a.length===0?s.jsx(d,{}):s.jsx(l,{autos:a}),e&&s.jsx(p,{})]})};export{u as default};
